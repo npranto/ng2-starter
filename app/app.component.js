ceb9e4c8ff8289e1a7b63502a42cc245357bee42
Nazmuz Shakib Pranto <npranto@gmail.com>
2016-12-23 01:27:29 -0500

    removes bundler files and updates .gitignore

--- app/app.component.js
+++ app/app.component.js
@@ -24,7 +24,7 @@ System.register(['angular2/core'], function(exports_1, context_1) {
                 AppComponent = __decorate([
                     core_1.Component({
                         selector: 'app',
-                        template: '<h1>Welcome to Angular 2</h1>'
+                        template: '<h1>Welcome to Angular 2!</h1>'
                     }), 
                     __metadata('design:paramtypes', [])
                 ], AppComponent);
